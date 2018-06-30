56d15340e5a462b7aea78b7c9bbdd7e13324dd61
hanhb1011 <1hanhb@naver.com>
2018-06-30 21:23:45 +0900

    add aws sdk

---
+++ app/src/main/java/com/amazonaws/models/nosql/UsersDO.java
@@ -0,0 +1,48 @@
+package com.amazonaws.models.nosql;
+
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBAttribute;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBHashKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBIndexHashKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBIndexRangeKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBRangeKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBTable;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@DynamoDBTable(tableName = "test-mobilehub-2065962779-Users")
+
+public class UsersDO {
+    private String _userId;
+    private String _name;
+    private Double _age;
+
+    @DynamoDBHashKey(attributeName = "userId")
+    @DynamoDBAttribute(attributeName = "userId")
+    public String getUserId() {
+        return _userId;
+    }
+
+    public void setUserId(final String _userId) {
+        this._userId = _userId;
+    }
+    @DynamoDBRangeKey(attributeName = "name")
+    @DynamoDBAttribute(attributeName = "name")
+    public String getName() {
+        return _name;
+    }
+
+    public void setName(final String _name) {
+        this._name = _name;
+    }
+    @DynamoDBAttribute(attributeName = "age")
+    public Double getAge() {
+        return _age;
+    }
+
+    public void setAge(final Double _age) {
+        this._age = _age;
+    }
+
+}
--- app/src/main/java/com/example/hanhb/aws/MainActivity.java
+++ app/src/main/java/com/example/hanhb/aws/MainActivity.java
@@ -2,17 +2,49 @@ package com.example.hanhb.aws;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBMapper;
+
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.models.nosql.UsersDO;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Declare a DynamoDBMapper object
+    DynamoDBMapper dynamoDBMapper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AWSMobileClient.getInstance().initialize(this).execute();
 
+        // Instantiate a AmazonDynamoDBMapperClient
+        AmazonDynamoDBClient dynamoDBClient = new AmazonDynamoDBClient(AWSMobileClient.getInstance().getCredentialsProvider());
+        this.dynamoDBMapper = DynamoDBMapper.builder()
+                .dynamoDBClient(dynamoDBClient)
+                .awsConfiguration(AWSMobileClient.getInstance().getConfiguration())
+                .build();
 
-        
 
 
     }
+
+    public void createNews() {
+        final UsersDO userItem = new UsersDO();
+
+        userItem.setAge(27d);
+        userItem.setName("Hyobyung Han");
+        userItem.setUserId("?");
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                dynamoDBMapper.save(userItem);
+                // Item saved
+            }
+        }).start();
+    }
+
+
 }
